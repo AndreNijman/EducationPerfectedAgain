75ff3c6532357b307b6c92e73f5744c58459f4bc
Andre Nijman <andre.perth.2010@gmail.com>
2025-11-16 18:43:51 +0800

    Add semver dependency and integrate update check in app startup

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // index.js (entry point)
 const { start } = require('./bootstrap');
+const { checkForUpdatesAndPrompt } = require('./updateChecker');
 
 (async () => {
+  // Fire-and-forget update check; does not block app startup
+  try { checkForUpdatesAndPrompt(); } catch (_) {}
   await start();
 })();
